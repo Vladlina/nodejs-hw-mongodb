6c8f5999a41d7a60507b73a40f034ce077783696
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-12-17 22:05:53 +0100

    update contacts.js and adding packages

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -31,14 +31,13 @@ export const getContactById = async (contactId, userId) => {
   return contact;
 };
 
-export const createContact = async (payload, userId) => {
+export const createContact = async (payload) => {
   if (!payload.name || !payload.phoneNumber || !payload.contactType) {
     throw new Error(
       "Обов'язкові поля name, phoneNumber та contactType відсутні",
     );
   }
 
-  payload.userId = userId;
   const contact = await ContactsCollection.create(payload);
   return contact;
 };
